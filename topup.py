9f76e93851431f797be4226e69dd914c3864eabf
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2020-12-22 00:18:24 -0500

    Update topup.py

--- topup.py
+++ topup.py
@@ -3,20 +3,27 @@ import os
 
 SECONDS = 60*15  # How long to spend making a throwaway key
 
+
 if __name__=='__main__':
   write_key = os.environ.get('WRITE_KEY')
-  if write_key is not None:
-      mw = MicroWriter(write_key=write_key)
-      balance = float(mw.get_balance())
-      if balance<0:
-          print('Trying to top up balance for '+mw.animal, ' which is currently '+str(balance))
-          throwaway_key = mw.maybe_create_key(difficulty=11, seconds=SECONDS)
-          if throwaway_key is None:
-              print('Sorry, no luck this time')
-          else:
-              print('Hit the jackpot ... topping up balance for '+ animal)
-              mw.put_balance(source_write_key=throwaway_key, amount=256)
-              print('New balance is '+str(mw.get_balance()))
-      else:   
-          print('Balance for '+mw.animal+' is '+str(balance)+' so no need to top up.')
+  if ',' in write_key:
+      write_keys = write_key.split(',')
+  else:
+      write_keys = [ write_key ] 
+  
+  for write_key in write_keys:
+      if write_key is not None:
+          mw = MicroWriter(write_key=write_key)
+          balance = float(mw.get_balance())
+          if balance<0:
+              print('Trying to top up balance for '+mw.animal, ' which is currently '+str(balance))
+              throwaway_key = mw.maybe_create_key(difficulty=11, seconds=SECONDS)
+              if throwaway_key is None:
+                  print('Sorry, no luck this time')
+              else:
+                  print('Hit the jackpot ... topping up balance for '+ animal)
+                  mw.put_balance(source_write_key=throwaway_key, amount=256)
+                  print('New balance is '+str(mw.get_balance()))
+          else:   
+              print('Balance for '+mw.animal+' is '+str(balance)+' so no need to top up.')
            
