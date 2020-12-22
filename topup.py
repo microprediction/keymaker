75fddb8dcb0ef02d8744c609007c5e8532f6dba6
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2020-12-21 23:38:31 -0500

    Update topup.py

--- topup.py
+++ topup.py
@@ -1,7 +1,7 @@
 from microprediction import MicroWriter
 import os
 
-SECONDS = 60*15
+SECONDS = 60*15  # How long to spend making a throwaway key
 
 if __name__=='__main__':
   write_key = os.environ.get('WRITE_KEY')
@@ -10,12 +10,12 @@ if __name__=='__main__':
       balance = float(mw.get_balance())
       if balance<0:
           print('Trying to top up balance for '+mw.animal, ' which is currently '+str(balance))
-          throwaway_key = mw.maybe_create_key(difficulty=12, seconds=SECONDS)
+          throwaway_key = mw.maybe_create_key(difficulty=11, seconds=SECONDS)
           if throwaway_key is None:
               print('Sorry, no luck this time')
           else:
               print('Hit the jackpot ... topping up balance for '+ animal)
-              mw.put_balance(source_write_key=throwaway_key)
+              mw.put_balance(source_write_key=throwaway_key, amount=256)
               print('New balance is '+str(mw.get_balance())
           
            
